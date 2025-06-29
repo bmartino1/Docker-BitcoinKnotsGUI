4b9f43a2422d637f10484a7f2c95b2695306cc73
Brandon Martino <167354702+bmartino1@users.noreply.github.com>
2025-06-28 19:50:53 -0500

    Update startapp.sh
    minor fix allow autoupdate or stay on current running version.

--- startapp.sh
+++ startapp.sh
@@ -8,16 +8,20 @@ mkdir -p /config/.bitcoin
 sudo chown -R nobody:users /config/.bitcoin || true
 sudo chmod -R 777 /config/.bitcoin || true
 
-#Fix Permission for Build
+# Fix Permission for Build
 mkdir -p /config/bitcoin
 sudo chown -R nobody:users /config/bitcoin || true
 sudo chmod -R 777 /config/bitcoin || true
 
-# Run update
-sudo /build.sh
+# Conditionally run update if AUTOUPDATE=true
+if [ "${AUTOUPDATE,,}" = "true" ]; then
+    echo "AUTOUPDATE is true, running /build.sh to update the application..."
+    sudo /build.sh
+else
+    echo "AUTOUPDATE is not true, skipping /build.sh"
+fi
 
 # Start Bitcoin Knots GUI
-#exec /config/bitcoin/bin/bitcoin-qt -datadir=/config/.bitcoin
 while true; do
     echo "Starting Bitcoin GUI..."
     /config/bitcoin/bin/bitcoin-qt -datadir=/config/.bitcoin
